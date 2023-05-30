1c420832500fb90ea1f3804c2ad8794e18ab41d6
qianxl <qianxl@vastio.com>
2023-05-30 14:09:45 +0800

    xx

--- 01.内置模块/10-路由/基础/route.js
+++ 01.内置模块/10-路由/基础/route.js
@@ -18,18 +18,18 @@ function route(pathname, res) {
 }
 */
 const route = {
-  "/login": (res) => {
-    res.writeHead(200, {"Content-Type": "text/html;charset=utf8"})
-    res.write(fs.readFileSync("./static/login.html", "utf-8"))
-  },
-  "/home": (res) => {
-    res.writeHead(200, {"Content-Type": "text/html;charset=utf8"})
-    res.write(fs.readFileSync("./static/home.html", "utf-8"))
-  },
+  "/login": (res) => render(res, "./static/login.html"),
+  "/home": (res) => render(res, "./static/home.html"),
   "/404": (res) => {
     res.writeHead(404, {"Content-Type": "text/html;charset=utf8"})
     res.write(fs.readFileSync("./static/404.html", "utf-8"))
   },
+  "/favicon.ico": (res) => render(res, "./static/favicon.png", "image/png"),
+}
+
+function render(res, path, contentType = "text/html;charset=utf8") {
+  res.writeHead(200, {"Content-Type": contentType})
+  res.write(fs.readFileSync(path, "utf-8"))
 }
 
 module.exports = route
--- 01.内置模块/10-路由/基础/server.js
+++ 01.内置模块/10-路由/基础/server.js
@@ -3,7 +3,11 @@ const route = require("./route");
 
 http.createServer((req, res) => {
   const myUrl = new URL(req.url, "http://127.0.0.1")
-  route[myUrl.pathname](res)
+  try {
+    route[myUrl.pathname](res)
+  } catch (e) {
+    route['/404'](res)
+  }
   res.end()
 }).listen(3000, () => {
   console.log('server start at http://localhost:3000')
